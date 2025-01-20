358ce02f2bd6352c81ffc5d6580126662223b883
sebast213123 <juan.cadena.ramirez@udla.edu.ec>
2025-01-20 07:57:07 -0500

    Acceso a la Base de Datos

---
+++ MauiPruebaSC/Constants.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiPruebaSC
+{
+    public static class Constants
+    {
+        private const string NombreArchivoBaseDatosSebastCadena = "SQLitePaises";
+        public const SQLiteOpenFlags Flags =
+
+            SQLiteOpenFlags.ReadWrite |
+            SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+
+        public static string PathBaseDatos {
+
+            get
+            {
+                return Path
+                    .Combine(FileSystem.AppDataDirectory,NombreArchivoBaseDatosSebastCadena);
+            }
+        }
+    }
+}
