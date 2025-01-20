7cce18f0c74fbb6503f1b48980104e8f2ebc25bb
Juan Sebastian Cadena Ramirez <juan.cadena.ramirez@udla.edu.ec>
2025-01-20 14:14:52 -0500

    corrijo los metodos para el correcto inotifypropertychanged

--- MauiPruebaSC/ViewModels/Modelos/Paisesp1ViewModel.cs
+++ MauiPruebaSC/ViewModels/Modelos/Paisesp1ViewModel.cs
@@ -1,22 +1,46 @@
 ﻿using SQLite;
 using System.ComponentModel;
 
-
 namespace MauiPruebaSC.ViewModels
 {
     [Table("Paises")]
     public class Paisesp1ViewModel : INotifyPropertyChanged
     {
-        [PrimaryKey,AutoIncrement] 
-        public int _ID {  get; set; }
-        [Column("nombrepais"),Indexed]
-        private string? _NombrePaisSebastianCadena {  get; set; }
+        private int _id;
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get => _id;
+            set { _id = value; OnPropertyChanged(); }
+        }
 
-        private string? _BuscarResultado {  get; set; }
-        private string? _MostrarError {  get; set; }
+        private string _nombrePais;
+        [Column("nombrepais"), Indexed]
+        public string NombrePais
+        {
+            get => _nombrePais;
+            set { _nombrePais = value; OnPropertyChanged(); }
+        }
 
+        private string _buscarResultado;
+        public string BuscarResultado
+        {
+            get => _buscarResultado;
+            set { _buscarResultado = value; OnPropertyChanged(); }
+        }
 
+        private string _mostrarError;
+        public string MostrarError
+        {
+            get => _mostrarError;
+            set { _mostrarError = value; OnPropertyChanged(); }
+        }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
+
