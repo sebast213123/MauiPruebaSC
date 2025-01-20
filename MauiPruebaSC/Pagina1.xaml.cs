bebca27256aede9711fb12b69f2fb1f8d091616b
sebast213123 <juan.cadena.ramirez@udla.edu.ec>
2025-01-20 07:22:03 -0500

    Creacion View Model Inicial e implementación property changed

---
+++ MauiPruebaSC/Pagina1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiPruebaSC;
+
+public partial class Pagina1 : ContentPage
+{
+	public Pagina1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiPruebaSC/ViewModels/Paisesp1ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiPruebaSC.ViewModels
+{
+    public class Paisesp1ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
