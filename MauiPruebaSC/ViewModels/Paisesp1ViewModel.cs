fa82bddf7002babd2219db8ec4bcf6ac866ebfef
sebast213123 <juan.cadena.ramirez@udla.edu.ec>
2025-01-20 07:40:19 -0500

    Creacion Atributos

--- MauiPruebaSC/ViewModels/Paisesp1ViewModel.cs
+++ MauiPruebaSC/ViewModels/Paisesp1ViewModel.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace MauiPruebaSC.ViewModels
 {
     public class Paisesp1ViewModel : INotifyPropertyChanged
     {
+
+        private string? _NombrePaisSebastianCadena {  get; set; }
+
+        private string? _BuscarResultado {  get; set; }
+        private string? _MostrarError {  get; set; }
+
+
+
         public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
