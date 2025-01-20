7bcbf5ccd547f75af48f3a0fc8793ae5fd94595a
laboratorio <laboratorio@UPOAULA10202>
2025-01-20 07:02:18 -0500

    Agregar archivos de proyecto.

---
+++ MauiPruebaSC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPruebaSC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiPruebaSC/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPruebaSC
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
